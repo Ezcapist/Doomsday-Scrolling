d2936d01290ef994913b7d4f707e5c35d3ab1163
Ezcapist <ezraspillekom@gmail.com>
2025-06-01 12:05:32 +0200

    Level design

--- doomsday-scrolling/scripts/missile.gd
+++ doomsday-scrolling/scripts/missile.gd
@@ -6,13 +6,15 @@ func _ready():
 	var rng:= RandomNumberGenerator.new()
 	
 	var width = get_viewport().get_visible_rect().size[0]
+	var camera = get_viewport().get_camera_2d().global_position.x
 	var random_x = rng.randi_range(0, width)
 	var random_y = rng.randi_range(-350,-750)
-	position = Vector2(random_x, random_y) 
+	position = Vector2(random_x  + camera, random_y) 
+	print(position.x)
 
 #Movement Missiles
 func _process(delta):
-		position += Vector2(0, 1.0) * 100 * delta
+		position += Vector2(0, 1.0) * 200 * delta
 	
 	
 #Game Over
